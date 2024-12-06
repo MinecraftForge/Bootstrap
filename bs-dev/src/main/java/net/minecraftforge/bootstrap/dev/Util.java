d48331715a4dc5aaae740aaca5550b137b9f8b55
LexManos <LexManos@gmail.com>
2024-12-05 19:29:37 -0800

    Speed up dev classpath scanning by only walking the folder e explicitly want.

--- bs-dev/src/main/java/net/minecraftforge/bootstrap/dev/Util.java
+++ bs-dev/src/main/java/net/minecraftforge/bootstrap/dev/Util.java
@@ -21,7 +21,10 @@ class Util {
     private static final Name AUTOMATIC_MODULE_NAME = new Name("Automatic-Module-Name");
     private static final Name MULTI_RELEASE         = new Name("Multi-Release");
     private static final Name FORGE_MODULE_LAYER    = new Name("Forge-Module-Layer");
-    private static final String VERSION_DIR         = "META-INF/versions/";
+    private static final String META_INF            = "META-INF";
+    private static final String MANIFEST            = "MANIFEST.MF";
+    private static final String VERSIONS            = "versions";
+    private static final String VERSION_DIR         = META_INF + '/' + VERSIONS + '/';
     private static final String MODULE_INFO         = "module-info.class";
 
     static ModuleVersion findModuleName(Collection<Path> paths) {
@@ -50,57 +53,80 @@ class Util {
             var infos = new ArrayList<InfoData>();
 
             if (Files.isDirectory(path)) {
-                var manifest = Files.walk(path)
-                    .filter(p -> JarFile.MANIFEST_NAME.equalsIgnoreCase(path.relativize(p).toString().replace('\\', '/')))
-                    .findFirst()
-                    .orElse(null);
+                var meta_inf = findInsensitive(path, META_INF);
+                var manifest = findInsensitive(meta_inf, MANIFEST);
 
                 if (manifest != null && Files.exists(manifest)) {
                     try (var is = Files.newInputStream(manifest)) {
                         mf = new Manifest(is);
                     }
                 }
-                var paths = Files.walk(path).filter(p -> p.toString().endsWith(MODULE_INFO)).toList();
-                for (var p : paths) {
-                    var relative = path.relativize(p).toString().replace('\\', '/');
-                    int version = 0;
-                    if (relative.startsWith(VERSION_DIR)) {
-                        int idx = relative.indexOf('/', VERSION_DIR.length());
-                        var ver = relative.substring(VERSION_DIR.length(), idx);
-                        version = Integer.parseInt(ver);
+
+                var module_info = path.resolve(MODULE_INFO);
+                if (Files.exists(module_info)) {
+                    infos.add(new InfoData(0, Files.readAllBytes(path)));
+                }
+
+                if (isMultiRelease(mf)) {
+                    var versions = findInsensitive(meta_inf, VERSIONS);
+                    if (versions != null) {
+                        Files.list(versions)
+                        .forEach(v -> {
+                            try {
+                                int version = Integer.parseInt(v.getFileName().toString());
+                                var mod_info = v.resolve(MODULE_INFO);
+                                if (version <= Runtime.version().feature() && Files.exists(mod_info))
+                                    infos.add(new InfoData(version, Files.readAllBytes(mod_info)));
+                            } catch (NumberFormatException e) {
+                                // If its not a numerical directory we don't care
+                            } catch (IOException e) {
+                                sneak(e);
+                            }
+                        });
                     }
-                    infos.add(new InfoData(version, Files.readAllBytes(p)));
                 }
             } else {
-                // I would use JarFile here and let the JVM handle all this, but it only works on File objects not Paths
+                // I would use JarFile here and let the JVM handle all this.
+                // but it only works on File objects not Paths
+                // Normal java gets around this by extracting all non-file paths
+                // to a temp directory and opening them as normal files.
+                // I do not want to do this. So this is what you get.
                 try (var zip = new ZipInputStream(Files.newInputStream(path))) {
                     ZipEntry entry;
                     while ((entry = zip.getNextEntry()) != null) {
                         var name = entry.getName();
-                        if (JarFile.MANIFEST_NAME.equalsIgnoreCase(name)) {
+                        if (mf == null && JarFile.MANIFEST_NAME.equalsIgnoreCase(name)) {
                             mf = new Manifest(zip);
                         } else if (name.endsWith(MODULE_INFO)) {
                             int version = 0;
                             if (name.startsWith(VERSION_DIR)) {
+                                if (!isMultiRelease(mf))
+                                    continue;
+
                                 int idx = name.indexOf('/', VERSION_DIR.length());
                                 var ver = name.substring(VERSION_DIR.length(), idx);
-                                version = Integer.parseInt(ver);
+                                try {
+                                    version = Integer.parseInt(ver);
+                                } catch (NumberFormatException e) {
+                                    // If its not a numerical directory we don't care
+                                    version = Integer.MAX_VALUE;
+                                }
                             }
-                            infos.add(new InfoData(version, zip.readAllBytes()));
+                            if (version <= Runtime.version().feature())
+                                infos.add(new InfoData(version, zip.readAllBytes()));
                         }
                     }
                 }
             }
 
             InfoData info = null;
-            if (mf != null && Boolean.parseBoolean(mf.getMainAttributes().getValue(MULTI_RELEASE))) {
+            if (infos.size() == 1) {
+                info = infos.get(0);
+            } else if (infos.size() > 1) {
                 info = infos.stream()
                     .sorted((a, b) -> b.version - a.version)
-                    .filter(a -> a.version <= Runtime.version().feature())
                     .findFirst()
                     .orElse(null);
-            } else {
-                info = infos.stream().filter(i -> i.version == 0).findFirst().orElse(null);
             }
 
             String name = _default;
@@ -121,6 +147,38 @@ class Util {
             return new ModuleVersion(name, version, layer);
         } catch (IOException e) {
             return sneak(e);
+        }
+    }
+
+    private static Path findInsensitive(Path root, String name) {
+        if (root == null)
+            return null;
+
+        // Fast path, its the correct case or on a file system that doesn't care about case
+        var child = root.resolve(name);
+        if (Files.exists(child))
+           return child;
+
+        try {
+            // If we can't find it fall back to listing all files and manually check, its slow but whatever
+            return Files.list(root)
+                .filter(p -> name.equalsIgnoreCase(p.getFileName().toString()))
+                .findFirst()
+                .orElse(null);
+        } catch (IOException e) {
+            return sneak(e);
+        }
+    }
+
+    private static boolean isMultiRelease(Manifest mf) {
+        return mf != null && Boolean.parseBoolean(mf.getMainAttributes().getValue(MULTI_RELEASE));
+    }
+
+    public static void main(String[] args) {
+        var p = Path.of("Z:\\Projects\\Forge_1214\\projects\\forge\\bin\\main");
+        for (int x = 0; x < 100; x++) {
+            var mod = findModuleName(p);
+            System.out.println(mod);
         }
     }
 
